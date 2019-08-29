f51d0feead7ba4a6e54e77671119ecad5a8fca50
Admin <ahz_393123108@163.com>
2019-08-29 19:48:01 +0800

    1111

--- src/chitu/demo/MyDemo.java
+++ src/chitu/demo/MyDemo.java
@@ -2,15 +2,6 @@ package chitu.demo;
 
 public class MyDemo {
     public static void main(String[] args) {
-        //第一次提交
-        //第二次提交
-        //第三次提交
-        //第四次提交
-        //第五次提交
-        //111
-        //222
-        //333
-        //444
         System.out.println("This is my first demo...");
     }
 }
